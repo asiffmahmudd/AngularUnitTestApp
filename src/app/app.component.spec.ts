07305043c73b648fbab1dbaa67456ba3f75a8459
Asif Mahmud <Asif.Mahmud@soti.net>
2023-11-09 11:54:32 -0500

    test files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Addition } from './Calculator';
 
 describe('AppComponent', () => {
+  let component = new AppComponent
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -13,6 +15,19 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
+  let count = 1;
+  afterEach(() => {
+    console.log(count + " test done");
+    count++;
+  })
+
+  beforeAll(() => {
+    console.log("Before starting test")
+  })
+
+  afterAll(() => {
+    console.log("all test done");
+  })
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -32,4 +47,46 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('AngularUnitTestApp app is running!');
   });
+
+  it('My testcase', () => {
+    expect(true).toBe(true);
+  });
+
+  it('Show Alert message', () => {
+    expect(component.ShowMessage("Hello")).toBe("Hello");
+  })
+
+  it('Show the Addition result', () => {
+    expect(Addition(10, 20)).toBe(30);
+  })
+
+  it('ToBe and ToEqual Test Case', () => {
+    var a = 'Hello';
+    var b = 'Hello';
+    var c = [1];
+    var d = [1];
+    var e = true;
+    var f = false;
+    var obj = {
+      foo: 'asd'
+    }
+    var myFunction = (function() {})();
+
+    var undefinedStr;
+
+    expect(a).toBe(b);
+    expect(c).toEqual(d);
+    expect(e).toBeTrue();
+    expect(f).toBeFalse();
+    expect(a).toBeTruthy();
+    expect(f).toBeFalsy();
+    expect(myFunction).toBeUndefined();
+    expect(obj.foo).not.toBeUndefined();
+    expect(undefinedStr).toBeUndefined();
+    expect(null).toBeNull();
+    expect([1,2,3]).toContain(2);
+
+  })
+
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularUnitTestApp';
+
+  ShowMessage(msg: string): string {
+    return msg;
+  }
 }
